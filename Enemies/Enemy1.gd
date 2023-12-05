109e4a19100f58e40b4db23d3cd7f2d13ef53c4e
Darius Wolfe <128618957+Cineros@users.noreply.github.com>
2023-12-04 22:16:15 -0500

    Catch Up work

--- Enemies/Enemy1.gd
+++ Enemies/Enemy1.gd
@@ -1,7 +1,7 @@
 #Made By Hunter Flati
 extends CharacterBody2D
 
-@export var speed = 100
+@export var speed = 200
 
 func _process(delta):
 	get_parent().set_progress(get_parent().get_progress() + speed*delta)
