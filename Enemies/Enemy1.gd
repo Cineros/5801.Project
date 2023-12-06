f511f7a004c63585bfa0383a84d122f0cfcad314
Darius Wolfe <128618957+Cineros@users.noreply.github.com>
2023-12-06 15:03:31 -0500

    still working on spawning.

--- Enemies/Enemy1.gd
+++ Enemies/Enemy1.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 @export var speed = 200
 @export var health = 2
+var health_set = false
 
 func destroy():
 	queue_free()
@@ -11,3 +12,8 @@ func _process(delta):
 	get_parent().set_progress(get_parent().get_progress() + speed*delta)
 	if health <= 0:
 		destroy()
+
+func _on_set_health(hp):
+	if !health_set:
+			health = hp
+			health_set = true
