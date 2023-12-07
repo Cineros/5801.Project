bf382807cc6c85c03135085414f34642978bd489
Hflati <147087305+Hflati@users.noreply.github.com>
2023-12-07 15:05:44 -0500

    A couple fixes

--- Menus/MapButton.gd
+++ Menus/MapButton.gd
@@ -4,7 +4,7 @@ extends Control
 @export_file var map_path
 
 var original_size := scale
-var growth := Vector2(1.1, 1.1)
+var growth := Vector2(1.02, 1.02)
 
 func button_grow(end: Vector2, duration: float):
 	var tween := create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
