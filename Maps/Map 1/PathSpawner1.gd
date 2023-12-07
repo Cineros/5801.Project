78db95fd33fdd13f5f4a6cf57f3ea7d783416a2f
Darius Wolfe <128618957+Cineros@users.noreply.github.com>
2023-12-07 18:37:25 -0500

    Still fixing spawning.

--- Maps/Map 1/PathSpawner1.gd
+++ Maps/Map 1/PathSpawner1.gd
@@ -4,7 +4,7 @@ extends Node2D
 @onready var path = preload("res://Maps/Map 1/Map1Path.tscn")
 @onready var OnePath = preload("res://Maps/Map 1/Map1Path1.tscn")
 var counter = 0
-var byte_count = 0
+var byte_count = 1
 var round_count = 0
 var enemy_tickets: int = 12
 var temp_tik
@@ -13,81 +13,71 @@ var enemy_scene0 = preload("res://Enemies/Enemy0.tscn")
 var enemy_scene1 = preload("res://Enemies/Enemy1.tscn")
 
 
+
+
 signal set_health
 
 func _on_timer_timeout():
 	print(temp_tik)
-	var tempPath = path.instantiate()
-	#Need to think of a way to make this work better not just spamming 0s. //Darius
-	#var enemy0 = enemy_scene0.instantiate()
-	#var enemy1 = enemy_scene1.instantiate()
-	if temp_tik > 0 or counter < 8:
-		if (temp_tik % 2 == 0 and temp_tik >= 128) or temp_tik >= 128 and counter == 0:
-			emit_signal("set_health", 128)
-			temp_tik -= 128
-		else:
-			add_child(tempPath)
-			#temp_tik - 0
-		if (temp_tik % 2 == 0 and temp_tik >= 64) or temp_tik >= 64 and counter == 1:
-			emit_signal("set_health", 64)
-			temp_tik -= 64
-		else:
-			add_child(tempPath)
-			#temp_tik - 0
-		if (temp_tik % 2 == 0 and temp_tik >= 32) or temp_tik >= 32 and counter == 2:
-			emit_signal("set_health", 32)
-			temp_tik -= 32
-		else:
-			add_child(tempPath)
-			#temp_tik - 0
-		if (temp_tik % 2 == 0 and temp_tik >= 16) or temp_tik >= 16 and counter == 3:
-			emit_signal("set_health", 16)
-			temp_tik -= 16
-		else:
-			add_child(tempPath)
-			#temp_tik - 0
-		if (temp_tik % 2 == 0 and temp_tik >= 8) or temp_tik >= 8 and counter == 4:
-			emit_signal("set_health", 8)
-			temp_tik -= 8
-			print(temp_tik)
-		else:
-			add_child(tempPath)
-			#temp_tik - 0
-		if (temp_tik % 2 == 0 and temp_tik >= 4) or temp_tik >= 4 and counter == 5:
-			emit_signal("set_health", 4)
-			temp_tik -= 4
-			print(temp_tik)
-		else:
-			add_child(tempPath)
-			#temp_tik - 0
-		if (temp_tik % 2 == 0 and temp_tik >= 2) or temp_tik >= 2 and counter == 6:
-			emit_signal("set_health", 2)
-			temp_tik -= 2
-		else:
-			add_child(tempPath)
-			#temp_tik - 0
-		if temp_tik == 1:
-			emit_signal("set_health", 1)
-			temp_tik -= 1
-			if byte_count == 2:
-				get_node("Timer").stop()
-		else:
-			add_child(tempPath)
-			if byte_count == 2:
-				get_node("Timer").stop()
+	
 	if temp_tik < 512:
 		temp_tik = enemy_tickets/2
 	else:
 		temp_tik = enemy_tickets/16
 		if bits_needed != 8:
 			bits_needed = 8
+
+	if temp_tik > 0 or counter < 8:
+		if (temp_tik % 2 == 0 and temp_tik >= 128) or temp_tik >= 128 and counter == 0:
+			spawnOne(128)
+		elif counter == 0:
+			spawnZero()
+		if (temp_tik % 2 == 0 and temp_tik >= 64) or temp_tik >= 64 and counter == 1:
+			spawnOne(64)
+		elif counter == 1:
+			spawnZero()
+		if (temp_tik % 2 == 0 and temp_tik >= 32) or temp_tik >= 32 and counter == 2:
+			spawnOne(32)
+		elif counter == 2:
+			spawnZero()
+		if (temp_tik % 2 == 0 and temp_tik >= 16) or temp_tik >= 16 and counter == 3:
+			spawnOne(16)
+		elif counter == 3:
+			spawnZero()
+		if (temp_tik % 2 == 0 and temp_tik >= 8) or temp_tik >= 8 and counter == 4:
+			spawnOne(8)
+		elif counter == 4:
+			spawnZero()
+		if (temp_tik % 2 == 0 and temp_tik >= 4) or temp_tik >= 4 and counter == 5:
+			spawnOne(4)
+		elif counter == 5:
+			spawnZero()
+		if (temp_tik % 2 == 0 and temp_tik >= 2) or temp_tik >= 2 and counter == 6:
+			spawnOne(2)
+		elif counter == 6:
+			spawnZero()
+		if temp_tik == 1:
+			spawnOne(1)
+			if byte_count == bits_needed:
+				get_node("Timer").stop()
+		elif counter == 7:
+			spawnZero()
+			if byte_count == bits_needed:
+				get_node("Timer").stop()
 	counter += 1
 	if counter > 7:
 		byte_count += 1
 		counter = 0
-	#for i in bits_needed: #for loop to run (AS MANY TIMES AT bits_needed), making sure it spawns at least 16 numbers.
-		#while temp_tik > 0:
-			#pass
+
+func spawnOne(health):
+	var one_path = OnePath.instantiate()
+	add_child(one_path)
+	emit_signal("set_health", health)
+	temp_tik -= health
+	
+func spawnZero():
+	var tempPath = path.instantiate()
+	add_child(tempPath)
 	
 
 func _on_interface_start_round():
@@ -99,9 +89,3 @@ func _on_interface_start_round():
 		enemy_tickets * 1.2
 	temp_tik = enemy_tickets
 	print(enemy_tickets)
-
-
-func _on_set_health(EHP):
-	var one_path = OnePath.instantiate()
-	add_child(one_path)
-	
