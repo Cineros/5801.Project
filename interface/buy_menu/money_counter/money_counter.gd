eefb740c99a76af1cfcbc444d64cc8bc4486604e
Hflati <147087305+Hflati@users.noreply.github.com>
2023-12-11 23:32:44 -0500

    Map2 Edit

--- interface/buy_menu/money_counter/money_counter.gd
+++ interface/buy_menu/money_counter/money_counter.gd
@@ -24,14 +24,14 @@ func _on_interface_add_score(score):
 	wallet += score
 
 
-func _on_buy_grid_2_money_change(price):
-	wallet -= price
-	if wallet < 0:
-		$Label.text = "0"
-		wallet = 0
-	else:
-		$Label.text = str(wallet)
-	emit_signal("cash_amount",wallet)
+#func _on_buy_grid_2_money_change(price):
+	#wallet -= price
+	#if wallet < 0:
+		#$Label.text = "0"
+		#wallet = 0
+	#else:
+		#$Label.text = str(wallet)
+	#emit_signal("cash_amount",wallet)
 
 
 func _on_start_button_2_money(counter):
--- interface/buy_menu/tower_grid.gd
+++ interface/buy_menu/tower_grid.gd
@@ -32,18 +32,18 @@ func _on_pointer_button_tower_bought(price):
 	emit_signal("money_change", price)
 
 
-func _on_money_counter_2_cash_amount(cash):
-	if cash < $pointer_button.price:
-		$pointer_button.disabled = true
-	else:
-		$pointer_button.disabled = false
+#func _on_money_counter_2_cash_amount(cash):
+	#if cash < $pointer_button.price:
+		#$pointer_button.disabled = true
+	#else:
+		#$pointer_button.disabled = false
 		
-	if cash < $Arrow_button.price:
-		$Arrow_button.disabled = true
-	else:
-		$Arrow_button.disabled = false
+	#if cash < $Arrow_button.price:
+		#$Arrow_button.disabled = true
+	#else:
+		#$Arrow_button.disabled = false
 		
-	if cash < $cursor_button.price:
-		$cursor_button.disabled = true
-	else:
-		$cursor_button.disabled = false
+	#if cash < $cursor_button.price:
+		#$cursor_button.disabled = true
+	#else:
+		#$cursor_button.disabled = false
